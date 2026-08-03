07bc39e62f8f93c8a5b6d2f83825a20692615aa2
nyattic <285356705+nyattic@users.noreply.github.com>
2026-08-03 17:41:07 +0900

    Add Clang-Tidy validation

--- cmake/WagleTools.cmake
+++ cmake/WagleTools.cmake
@@ -36,6 +36,55 @@ if(WOBBLEPAINT_CLANG_FORMAT)
     )
 endif()
 
+find_program(
+    WOBBLEPAINT_CLANG_TIDY
+    NAMES clang-tidy
+    HINTS
+    "/opt/homebrew/opt/llvm/bin"
+    "/usr/local/opt/llvm/bin"
+)
+find_program(
+    WOBBLEPAINT_RUN_CLANG_TIDY
+    NAMES run-clang-tidy run-clang-tidy.py
+    HINTS
+    "/opt/homebrew/opt/llvm/bin"
+    "/usr/local/opt/llvm/bin"
+)
+if(WOBBLEPAINT_CLANG_TIDY AND WOBBLEPAINT_RUN_CLANG_TIDY)
+    set(WOBBLEPAINT_CLANG_TIDY_ARGUMENTS)
+    if(APPLE)
+        execute_process(
+            COMMAND xcrun --sdk macosx --show-sdk-path
+            OUTPUT_VARIABLE WOBBLEPAINT_MACOS_SDK_PATH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            RESULT_VARIABLE WOBBLEPAINT_MACOS_SDK_RESULT
+        )
+        if(WOBBLEPAINT_MACOS_SDK_RESULT EQUAL 0)
+            list(
+                APPEND
+                WOBBLEPAINT_CLANG_TIDY_ARGUMENTS
+                -extra-arg=-isysroot
+                "-extra-arg=${WOBBLEPAINT_MACOS_SDK_PATH}"
+            )
+        endif()
+    endif()
+    add_custom_target(
+        wobblepaint_tidy
+        COMMAND
+        "${WOBBLEPAINT_RUN_CLANG_TIDY}"
+        -p
+        "${CMAKE_BINARY_DIR}"
+        -clang-tidy-binary
+        "${WOBBLEPAINT_CLANG_TIDY}"
+        -quiet
+        ${WOBBLEPAINT_CLANG_TIDY_ARGUMENTS}
+        "^${CMAKE_CURRENT_SOURCE_DIR}/(src|tests|tools)/.*\\.(cpp|mm)$"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+        USES_TERMINAL
+        VERBATIM
+    )
+endif()
+
 if(APPLE)
     add_executable(
         wobblepaint_render_release_notes
